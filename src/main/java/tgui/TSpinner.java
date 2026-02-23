bd0ecc8855e9f2901ea3354cb4318c45044f21bb
Drew Elgar <drew_elgar@outlook.com>
2026-02-23 20:51:03 +1000

    Timetable details window now has current values as defaults for title, start time, and end time.

--- src/main/java/tgui/TSpinner.java
+++ src/main/java/tgui/TSpinner.java
@@ -9,11 +9,19 @@ import java.awt.*;
 public class TSpinner extends JSpinner {
     public TSpinner(double stepSize) {
         super(new SpinnerNumberModel(0.0, 0.0, 24.0, stepSize));
+        setStyle();
+    }
+
+    public TSpinner(double value, double stepSize) {
+        super(new SpinnerNumberModel(value, 0.0, 24.0, stepSize));
+        setStyle();
+    }
+
+    private void setStyle() {
         this.setBackground(Color.GRAY);
         this.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createLineBorder(Color.BLACK, 1, true),
                 BorderFactory.createEmptyBorder(4, 4, 4, 4)
         ));
-
     }
 }
